7f3eed4dae51e164c0cda16adf50a677cfea5699
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2026-01-05 21:46:43 +0530

    Bump version: 0.5.0 → 0.5.1

--- recallrai/utils/http_client.py
+++ recallrai/utils/http_client.py
@@ -44,7 +44,7 @@ class HTTPClient:
                 "X-Recallr-Project-Id": self.project_id,
                 "Content-Type": "application/json",
                 "Accept": "application/json",
-                "User-Agent": f"RecallrAI-Python-SDK/0.5.0",
+                "User-Agent": f"RecallrAI-Python-SDK/0.5.1",
             },
         )
 
